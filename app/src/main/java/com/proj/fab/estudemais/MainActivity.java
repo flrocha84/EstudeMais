5f1028efd65be4678a81148d62e40754b70caaf3
Fabio Rocha <fabiolr@gmail.com>
2022-03-23 23:35:59 -0300

    tirei o binding

--- app/src/main/java/com/proj/fab/estudemais/MainActivity.java
+++ app/src/main/java/com/proj/fab/estudemais/MainActivity.java
@@ -2,25 +2,22 @@ package com.proj.fab.estudemais;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.Menu;
+
 import android.widget.FrameLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
+
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.proj.fab.estudemais.databinding.ActivityMain2Binding;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -51,23 +48,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMain2Binding binding;
+    //private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar2 = findViewById(R.id.toolbar);
+        toolbar2.setTitle("Exame");
+        setSupportActionBar(toolbar2);
+       // binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
 
         bottomNavigationView = findViewById(R.id.bottom_nav_bar);
         main_frame = findViewById(R.id.main_frame);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
 
-        setSupportActionBar(binding.appBarMain2.toolbar);
 
-        DrawerLayout drawer = binding.drawerLayout;
+
+      //  DrawerLayout drawer = binding.drawerLayout;
 
 
 
