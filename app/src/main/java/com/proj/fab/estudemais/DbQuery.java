d31c81edc14fe26d940c17eda7a8e60578e05bcd
Fabio Rocha <fabiolr@gmail.com>
2022-04-09 23:22:07 -0300

    inicio da leaderBoard

--- app/src/main/java/com/proj/fab/estudemais/DbQuery.java
+++ app/src/main/java/com/proj/fab/estudemais/DbQuery.java
@@ -3,13 +3,16 @@ package com.proj.fab.estudemais;
 import androidx.annotation.NonNull;
 import androidx.collection.ArrayMap;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
@@ -37,8 +40,12 @@ public class DbQuery {
 
    public static List<QuestionModel> g_quesList = new ArrayList<>();
 
+   public static List<RankModel> g_usersList = new ArrayList<>();
+   public static int g_usersCount =0;
+   public static boolean isMeOnTopList =false;
+
    public static ProfileModel myProfile = new ProfileModel("NA",null,null);
-   public static RankModel myPerformace = new RankModel(0,-1);
+   public static RankModel myPerformace = new RankModel(null,0,-1);
 
     public static final int NOT_VISITED =0;
     public static final int UNANSWERED =1;
@@ -160,6 +167,72 @@ public class DbQuery {
                    }
                });
    }
+
+   public static void getTopUsers(MyCompleteListener completeListener)
+   {
+       g_usersList.clear();
+
+       String myUID=FirebaseAuth.getInstance().getUid();
+
+       g_firestore.collection("USERS")
+               .whereGreaterThan("TOTAL_SCORE",0)
+               .orderBy("TOTAL_SCORE", Query.Direction.DESCENDING)
+               .limit(20)
+               .get()
+               .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                   @Override
+                   public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        int rank =1;
+                       for (QueryDocumentSnapshot doc : queryDocumentSnapshots)
+                       {
+                           g_usersList.add(new RankModel(
+                                   doc.getString("NAME"),
+                                    doc.getLong("TOTAL_SCORE").intValue(),
+                                    rank
+                           ));
+
+                           if (myUID.compareTo(doc.getId()) ==0)
+                           {
+                               isMeOnTopList=true;
+                               myPerformace.setRank(rank);
+                           }
+
+
+                           rank++;
+                       }
+                        completeListener.onSuccess();
+                   }
+               })
+               .addOnFailureListener(new OnFailureListener() {
+                   @Override
+                   public void onFailure(@NonNull Exception e) {
+                        completeListener.onFailure();
+                   }
+               });
+
+   }
+
+   public static void getUsersCount(MyCompleteListener completeListener)
+   {
+       g_firestore.collection("USERS").document("TOTAL_USERS")
+               .get()
+               .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                   @Override
+                   public void onSuccess(DocumentSnapshot documentSnapshot) {
+                       g_usersCount=documentSnapshot.getLong("COUNT").intValue();
+
+                       completeListener.onSuccess();
+                   }
+               })
+               .addOnFailureListener(new OnFailureListener() {
+                   @Override
+                   public void onFailure(@NonNull Exception e) {
+                        completeListener.onFailure();
+                   }
+               })
+
+   }
+
 
    public static void saveResult(int score, MyCompleteListener completeListener)
    {
@@ -300,7 +373,17 @@ public class DbQuery {
             loadCategories(new MyCompleteListener() {
                 @Override
                 public void onSuccess() {
-                    getUserData(completeListener);
+                    getUserData(new MyCompleteListener() {
+                        @Override
+                        public void onSuccess() {
+                            getUsersCount(completeListener);
+                        }
+
+                        @Override
+                        public void onFailure() {
+                            completeListener.onFailure();
+                        }
+                    });
                 }
 
                 @Override
--- app/src/main/java/com/proj/fab/estudemais/LeaderBoardFragment.java
+++ app/src/main/java/com/proj/fab/estudemais/LeaderBoardFragment.java
@@ -3,62 +3,61 @@ package com.proj.fab.estudemais;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LeaderBoardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
+
+
+
+
 public class LeaderBoardFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+ private TextView totalUsersTV, myImgTextTV,myScoreTV,myRankTV;
+ private RecyclerView usersView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public LeaderBoardFragment() {
-        // Required empty public constructor
+        public LeaderBoardFragment() {
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LeaderBoardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LeaderBoardFragment newInstance(String param1, String param2) {
-        LeaderBoardFragment fragment = new LeaderBoardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leader_board, container, false);
+        View view= inflater.inflate(R.layout.fragment_leader_board, container, false);
+
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Classificação");
+
+
+        initViews(view);
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        usersView.setLayoutManager(layoutManager);
+
+        totalUsersTV.setText("Total de usuários: ");
+
+
+        return view;
+
+    }
+
+
+    private void initViews(View view)
+    {
+        totalUsersTV=view.findViewById(R.id.total_users);
+        myImgTextTV=view.findViewById(R.id.img_text);
+        myScoreTV=view.findViewById(R.id.total_score);
+        myRankTV=view.findViewById(R.id.rank);
+        usersView=view.findViewById(R.id.users_view);
+
     }
 }
